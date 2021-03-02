b11303b878fa241402def77d11f643c38266e2b3
jaekook <jaekook4849@gmail.com>
2021-03-02 18:09:50 +0900

    Develop Web MVC

--- SpringIntroduction/src/main/java/jpabook/SpringIntroduction/controller/MemberController.java
+++ SpringIntroduction/src/main/java/jpabook/SpringIntroduction/controller/MemberController.java
@@ -1,8 +1,14 @@
 package jpabook.SpringIntroduction.controller;
 
+import jpabook.SpringIntroduction.domain.Member;
 import jpabook.SpringIntroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,33 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    /**
+     * 회원 등록
+     */
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    /**
+     * 회원 조회
+     */
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
