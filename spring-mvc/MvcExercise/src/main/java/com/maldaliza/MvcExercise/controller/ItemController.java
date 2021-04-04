e9f4b538dffb0256417466e4fbf8a84dbe8b7931
jaekook <jaekook4849@gmail.com>
2021-04-05 08:03:49 +0900

    Show Item Details and Add Item

--- spring-mvc/MvcExercise/src/main/java/com/maldaliza/MvcExercise/controller/ItemController.java
+++ spring-mvc/MvcExercise/src/main/java/com/maldaliza/MvcExercise/controller/ItemController.java
@@ -3,28 +3,82 @@ package com.maldaliza.MvcExercise.controller;
 import com.maldaliza.MvcExercise.AppConfig;
 import com.maldaliza.MvcExercise.domain.Item;
 import com.maldaliza.MvcExercise.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
 @RequestMapping("/items")
+@Slf4j
 public class ItemController {
 
     ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
     ItemRepository itemRepository = applicationContext.getBean("itemRepository", ItemRepository.class);
 
+    /**
+     * 상품 목록
+     * @param model
+     * @return
+     */
     @GetMapping
     public String items(Model model) {
+
+        log.info("Show Item List");
+
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
+
         return "/items";
+    }
+
+    /**
+     * 상품 상세보기
+     * @param itemId
+     * @param model
+     * @return
+     */
+    @GetMapping("{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+
+        log.info("Show Item Details about ID={}", itemId);
+
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "/item";
+    }
+
+    /**
+     * 상품 등록 (GET)
+     * @return
+     */
+    @GetMapping("/add")
+    public String addForm() {
+
+        log.info("Add Item");
+
+        return "/addItem";
+    }
+
+    /**
+     * 상품 등록 (POST)
+     * @param item
+     * @return
+     */
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item) {
+
+        log.info("Successfully Added Item");
+
+        itemRepository.save(item);
+
+        return "redirect:/items/" + item.getId();
     }
 
     @PostConstruct
