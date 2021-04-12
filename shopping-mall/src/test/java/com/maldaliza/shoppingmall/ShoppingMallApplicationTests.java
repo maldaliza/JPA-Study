6e16b68650a9cfc63f553ac330247c9989c715e4
jaekook <jaekook4849@gmail.com>
2021-04-12 10:18:30 +0900

    Create a Shopping Mall Project and Make Basic Settings

---
+++ shopping-mall/src/test/java/com/maldaliza/shoppingmall/ShoppingMallApplicationTests.java
@@ -0,0 +1,12 @@
+package com.maldaliza.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
