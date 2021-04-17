6237ca25232277cbc4cf296e181133c3ab1a057f
jaekook <jaekook4849@gmail.com>
2021-04-17 12:53:34 +0900

    Making Member Registration Process and Member List

---
+++ shopping-mall/src/main/java/com/maldaliza/shoppingmall/controller/MemberController.java
@@ -0,0 +1,65 @@
+package com.maldaliza.shoppingmall.controller;
+
+import com.maldaliza.shoppingmall.domain.Address;
+import com.maldaliza.shoppingmall.domain.Member;
+import com.maldaliza.shoppingmall.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 회원 등록 폼 (GET)
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    /**
+     * 회원 등록 처리 (POST)
+     *
+     * @param form
+     * @param result
+     * @return
+     */
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/members";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
