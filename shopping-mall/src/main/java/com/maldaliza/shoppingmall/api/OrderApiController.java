de217f8e6b986c3e660c3d453ba11af4747914e1
jaekook <jaekook4849@gmail.com>
2021-05-14 08:42:31 +0900

    Convert Entity to DTO

--- shopping-mall/src/main/java/com/maldaliza/shoppingmall/api/OrderApiController.java
+++ shopping-mall/src/main/java/com/maldaliza/shoppingmall/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package com.maldaliza.shoppingmall.api;
 
+import com.maldaliza.shoppingmall.domain.Address;
 import com.maldaliza.shoppingmall.domain.Order;
 import com.maldaliza.shoppingmall.domain.OrderItem;
+import com.maldaliza.shoppingmall.domain.OrderStatus;
 import com.maldaliza.shoppingmall.repository.OrderRepository;
 import com.maldaliza.shoppingmall.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +23,7 @@ public class OrderApiController {
 
     /**
      * 엔티티를 직접 노출
+     *
      * @return
      */
     @GetMapping("/api/v1/orders")
@@ -32,5 +38,56 @@ public class OrderApiController {
             orderItems.stream().forEach(orderItem -> orderItem.getItem().getName());
         }
         return all;
+    }
+
+    /**
+     * 엔티티를 조회해서 DTO로 변환
+     * @return
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        // orders -> orderDto
+        List<OrderDto> result = orders.stream()
+                .map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+
+            // orderItems -> orderItemDto
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+        private String itemName;   // 상품명
+        private int orderPrice;    // 주문 가격
+        private int count;         // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
